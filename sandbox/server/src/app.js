70d480d401f687545a27aaa615b0b2e66fa15148
Ritik Kumar <ritik.kumar22@outlook.com>
2026-09-23 23:14:59 +0530

    feat(server): initialize server with Express and basic health check endpoint

---
+++ sandbox/server/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/sandbox/health", (req, res) => {
+    res.status(200).json({
+        message: "Sandbox api is healthy.",
+        status: "Ok"
+    });
+});
+
+export default app;
