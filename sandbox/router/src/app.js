efa820b22f55fef7d571a35a73319e68e0d0bb13
Ritik Kumar <ritik.kumar22@outlook.com>
2026-09-26 13:44:17 +0530

    feat(router): add proxy middleware for dynamic sandbox routing and create Dockerfile

--- sandbox/router/src/app.js
+++ sandbox/router/src/app.js
@@ -1,10 +1,25 @@
 import express from "express";
 import morgan from "morgan";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
+
+app.use((req, res, next) => {
+    const host = req.headers.host
+
+    const sandboxId = host.split(".")[0]
+
+    const target = `http://sandbox-service-${sandboxId}`
+
+    return createProxyMiddleware({
+        target, 
+        changeOrigin: true,
+        ws: true
+    }) (req, res, next);
+});
 
 app.get("/api/status/healthz", (req, res) => {
     res.status(200).json({ status: "ok" });
@@ -13,5 +28,7 @@ app.get("/api/status/healthz", (req, res) => {
 app.get("/api/status/readyz", (req, res) => {
     res.status(200).json({ status: "ready" });
 });
+
+
 
 export default app;
